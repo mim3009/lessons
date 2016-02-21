960cf9fcca55f4e5062f51b06aa3c23fcb756a01
Roman <mim.roma@mail.ru>
2016-02-21 23:04:37 +0200

    Update Create.js

--- уроки/javascript/Create.js
+++ уроки/javascript/Create.js
@@ -70,7 +70,7 @@ function removeDraw(){
 
 function creatingPoints(e){
 	var element = e.target || e.srcElement;
-	if(element == '[object HTMLHtmlElement]' || element == '[object SVGSVGElement]'){
+	if(element == '[object SVGSVGElement]'){
 		var div = document.createElement("div");
 		div.className = "movedElement";
 		div.style.background = "gray";
@@ -89,6 +89,18 @@ function creatingPoints(e){
 		div.oncontextmenu = changeColour;
 
 		document.body.appendChild(div);
+
+		var table = document.getElementById("pointsTable");
+		var row = table.insertRow(table.rows.length);
+		var cell0 = row.insertCell(0);
+		var cell1 = row.insertCell(1);
+		var cell2 = row.insertCell(2);
+		var cell3 = row.insertCell(3);
+		cell0.innerHTML = Number(div.style.left.replace(/\D+/g,"")) + Number(div.style.width.replace(/\D+/g,""))/2 + "px";
+		cell1.innerHTML = Number(div.style.top.replace(/\D+/g,"")) + Number(div.style.height.replace(/\D+/g,""))/2 + "px";
+		cell2.innerHTML = div.style.background;
+		cell3.innerHTML = Number(div.style.height.replace(/\D+/g,""))/2;
+		row.style.background = div.style.background; 
 	}
 	
 }
@@ -99,18 +111,85 @@ function changeColour(e){
 	var el = e.target || e.srcElement;
 	if(el.style.background == "gray"){
 		el.style.background = "red";
+		var tr = document.getElementById("pointsTable").getElementsByTagName('tr');
+		for(var i = 0; i < tr.length; i++){
+			var td = tr[i].getElementsByTagName('td');
+			for(var j = 0; j < td.length; j++){
+				if(Number(td[0].innerHTML.replace(/\D+/g,"")) == parseInt(Number(el.style.left.replace(/\D+/g,"")) + Number(el.style.width.replace(/\D+/g,""))/2) && Number(td[1].innerHTML.replace(/\D+/g,"")) == parseInt(Number(el.style.top.replace(/\D+/g,"")) + Number(el.style.height.replace(/\D+/g,""))/2)){
+					td[2].innerHTML = el.style.background;
+					tr[i].style.background = el.style.background;
+				}
+			}
+		}
 	}
 	else if(el.style.background == "red"){
 		el.style.background = "green";
+		var tr = document.getElementById("pointsTable").getElementsByTagName('tr');
+		for(var i = 0; i < tr.length; i++){
+			var td = tr[i].getElementsByTagName('td');
+			for(var j = 0; j < td.length; j++){
+				if(Number(td[0].innerHTML.replace(/\D+/g,"")) == parseInt(Number(el.style.left.replace(/\D+/g,"")) + Number(el.style.width.replace(/\D+/g,""))/2) && Number(td[1].innerHTML.replace(/\D+/g,"")) == parseInt(Number(el.style.top.replace(/\D+/g,"")) + Number(el.style.height.replace(/\D+/g,""))/2)){
+					td[2].innerHTML = el.style.background;
+					tr[i].style.background = el.style.background;
+				}
+			}
+		}
 	}
 	else{
+		var table = document.getElementById("pointsTable");
+		var tr = document.getElementById("pointsTable").getElementsByTagName('tr');
+		for(var i = 0; i < tr.length; i++){
+			var td = tr[i].getElementsByTagName('td');
+			for(var j = 0; j < td.length; j++){
+				if(Number(td[0].innerHTML.replace(/\D+/g,"")) == parseInt(Number(el.style.left.replace(/\D+/g,"")) + Number(el.style.width.replace(/\D+/g,""))/2) && Number(td[1].innerHTML.replace(/\D+/g,"")) == parseInt(Number(el.style.top.replace(/\D+/g,"")) + Number(el.style.height.replace(/\D+/g,""))/2)){
+					table.deleteRow(i);
+					break;
+				}
+			}
+		}
+
+		var rad1 = parseInt(Number(el.style.height.replace(/\D+/g,""))/2);
+		var svg = document.getElementsByTagName("svg");
+		var lines = document.getElementsByTagName("line");
+		var findedLines = new Array();
+		var j = 0;
+		for(var i = 0; i < lines.length; i++){
+			if(lines[i].getAttribute("x1") == (Number(el.style.left.replace(/\D+/g,""))-svg[0].offsetLeft+rad1) && lines[i].getAttribute("y1") == (Number(el.style.top.replace(/\D+/g,""))-svg[0].offsetTop+rad1)){
+				findedLines[j] = lines[i];
+				findedLines[j].setAttribute("ch", "1");
+				j++;
+			}
+			if(lines[i].getAttribute("x2") == (Number(el.style.left.replace(/\D+/g,""))-svg[0].offsetLeft+rad1) && lines[i].getAttribute("y2") == (Number(el.style.top.replace(/\D+/g,""))-svg[0].offsetTop+rad1)){
+				findedLines[j] = lines[i];
+				findedLines[j].setAttribute("ch", "2");
+				j++;
+			}
+		}
+		for(var i = 0; i < findedLines.length; i++){
+			svg[0].removeChild(findedLines[i]);
+		}
+	
 		document.body.removeChild(el);
-		console.log("element was deleted");
 	}
 	e.stopPropagation();
 }
 
 function changeRadius(elementToChange, event){
+	var tr = document.getElementById("pointsTable").getElementsByTagName('tr');
+	var findX = null;
+	var findY = null;
+	var findR = null;
+	for(var i = 0; i < tr.length; i++){
+		var td = tr[i].getElementsByTagName('td');
+		for(var j = 0; j < td.length; j++){
+			if(Number(td[0].innerHTML.replace(/\D+/g,"")) == parseInt(Number(elementToChange.style.left.replace(/\D+/g,"")) + Number(elementToChange.style.width.replace(/\D+/g,""))/2) && Number(td[1].innerHTML.replace(/\D+/g,"")) == parseInt(Number(elementToChange.style.top.replace(/\D+/g,"")) + Number(elementToChange.style.height.replace(/\D+/g,""))/2)){
+				findX = td[0];
+				findY = td[1];
+				findR = td[3];
+				break;
+			}
+		}
+	}
 	var radius = parseInt(Number(elementToChange.style.height.replace(/\D+/g,""))/2);
 	var startY = event.clientY;
 	var spY = Number(elementToChange.style.top.replace(/\D+/g,"")) + radius;
@@ -160,6 +239,9 @@ function changeRadius(elementToChange, event){
 			elementToChange.style.top = Number(elementToChange.style.top.replace(/\D+/g,"")) + cY + "px";
 			elementToChange.style.left = Number(elementToChange.style.left.replace(/\D+/g,"")) + cX + "px";
 		}
+		findX.innerHTML = parseInt(Number(elementToChange.style.left.replace(/\D+/g,"")) + Number(elementToChange.style.width.replace(/\D+/g,""))/2) + "px";
+		findY.innerHTML = parseInt(Number(elementToChange.style.top.replace(/\D+/g,"")) + Number(elementToChange.style.height.replace(/\D+/g,""))/2) + "px";
+		findR.innerHTML = parseInt(Number(elementToChange.style.height.replace(/\D+/g,""))/2);
 		if(e.stopPropagation) e.stopPropagation();
 		else e.cancelBubble = true;
 	}
@@ -175,13 +257,10 @@ function changeRadius(elementToChange, event){
 }
 
 function drawLine(el, event){
-	console.log("element downH =" + el);
 	var x1 = Number(el.style.left.replace(/\D+/g,""));
 	var y1 = Number(el.style.top.replace(/\D+/g,""));
-	var rad1 = (Number(el.style.height.replace(/\D+/g,""))/2);
-	console.log("x1 = " + x1);
-	console.log("y1 = " + y1);
-	console.log("rad1 = " + rad1)
+	var rad1 = parseInt(Number(el.style.height.replace(/\D+/g,""))/2);
+
 	document.addEventListener("mousemove", moveHandl, true);
 	document.addEventListener("mouseup", upHandl, true);
 
@@ -191,103 +270,6 @@ function drawLine(el, event){
 	if(event.preventDefault) event.preventDefault();
 	else event.returnValue = false;
 
-	function checkingMinusForSVG(a, b, r1, r2){
-		var res = a - b;
-		var resrad = r1 - r2;
-		if(resrad == 0){
-			if(res < 0){
-				return a + (r1+r2)/2;
-			}
-			else{
-				return b + (r1+r2)/2;
-			}
-		}
-		else if(resrad > 0){
-			if(res < 0){
-				return a + (r1+r2)/2 - resrad/2;
-			}
-			else{
-				return b + (r1+r2)/2 + resrad/2;
-			}
-		}
-		else{
-			if(res < 0){
-				return a + (r1+r2)/2 - resrad/2;
-			}
-			else{
-				return b + (r1+r2)/2 + resrad/2;
-			}
-		}
-		
-	}
-
-	function checkingMinusforLineOne(a, b, r1, r2){
-		var res = a - b;
-		var resrad = r1 - r2;
-		if(resrad == 0){
-			if(res < 0){
-				return Math.abs(a - b);
-			}
-			else{
-				return 0;
-			}
-		}
-		else if(resrad > 0){
-			if(res < 0){
-				return (Math.abs(a - b) + Math.abs(resrad));
-			}
-			else{
-				return 0;
-			}
-		}
-		else{
-			if(res < 0){
-				return (Math.abs(a - b) - Math.abs(resrad));
-			}
-			else{
-				return 0;
-			}
-		}
-	}
-
-	function checkingMinusforLineTwo(a, b, r1, r2){
-		var res = a - b;
-		var resrad = r1 - r2;
-		if(resrad == 0){
-			if(res < 0){
-				return 0;
-			}
-			else{
-				return Math.abs(a - b);
-			}
-		}
-		else if(resrad > 0){
-			if(res < 0){
-				return 0;
-			}
-			else{
-				return (Math.abs(a - b) - Math.abs(resrad));
-			}
-		}
-		else{
-			if(res < 0){
-				return 0;
-			}
-			else{
-				return (Math.abs(a - b) + Math.abs(resrad));
-			}
-		}
-	}
-
-	function checkZero(a1, a2, rad1, rad2){
-		if(Math.abs((a2+rad2)-(a1+rad1))>0){
-			return Math.abs((a2+rad2)-(a1+rad1));
-		}
-		else{
-			return 1;
-		}
-	}
-
 	function moveHandl(e){
 		if(!e) e = window.event;
 		
@@ -296,43 +278,41 @@ function drawLine(el, event){
 	}
 
 	function upHandl(e){
-		var svgs = document.getElementsByClassName('svg');
 		if(!e) e = window.event;
 		var element = e.target || e.srcElement;
-		console.log("element upH =" + element);
 		if(element == '[object HTMLDivElement]' && element != el){
 			var x2 = Number(element.style.left.replace(/\D+/g,""));
 			var y2 = Number(element.style.top.replace(/\D+/g,""));
-			var rad2 = (Number(element.style.height.replace(/\D+/g,""))/2);
-			console.log("x2 = " + x2);
-			console.log("y2 = " + y2);
-			console.log("rad2 = " + rad2);
-
-			var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-			svg.setAttribute("class", "svg");
-			svg.setAttribute("style", "height: " + checkZero(y1, y2, rad1, rad2) + "; width: " + checkZero(x1, x2, rad1, rad2) + "; position: absolute; left:" + (checkingMinusForSVG(x2, x1, rad1, rad2)) + "px; top:" + (checkingMinusForSVG(y2, y1, rad1, rad2)) + "px");
-
+			var rad2 = parseInt(Number(element.style.height.replace(/\D+/g,""))/2);
+			var svg = document.getElementsByTagName("svg");
+			var lines = document.getElementsByTagName("line");
 			var flag = 0;
-			for(var i = 0; i < svgs.length; i++){
-				if(Number(svgs[i].style.left.replace(/\D+/g,"")) == Number(svg.style.left.replace(/\D+/g,"")) && Number(svgs[i].style.top.replace(/\D+/g,"")) == Number(svg.style.top.replace(/\D+/g,"")) && Number(svgs[i].style.height.replace(/\D+/g,"")) == Number(svg.style.height.replace(/\D+/g,"")) && Number(svgs[i].style.width.replace(/\D+/g,"")) == Number(svg.style.width.replace(/\D+/g,""))){
+			for(var i = 0; i < lines.length; i++){
+				if((lines[i].getAttribute("x1") == (x1-svg[0].offsetLeft+rad1) && lines[i].getAttribute("y1") == (y1-svg[0].offsetTop+rad1) && lines[i].getAttribute("x2") == (x2-svg[0].offsetLeft+rad2) && lines[i].getAttribute("y2") == (y2-svg[0].offsetTop+rad2)) || (lines[i].getAttribute("x1") == (x2-svg[0].offsetLeft+rad2) && lines[i].getAttribute("y1") == (y2-svg[0].offsetTop+rad2) && lines[i].getAttribute("x2") == (x1-svg[0].offsetLeft+rad1) && lines[i].getAttribute("y2") == (y1-svg[0].offsetTop+rad1))){
 					flag = 1;
 				}
 			}
-
 			if(flag == 0){
 				var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-				line.setAttribute("class", "line");
-				line.setAttribute("x1", checkingMinusforLineOne(x2, x1, rad1, rad2));
-				line.setAttribute("y1", checkingMinusforLineOne(y2, y1, rad1, rad2));
-				line.setAttribute("x2", checkingMinusforLineTwo(x2, x1, rad1, rad2));
-				line.setAttribute("y2", checkingMinusforLineTwo(y2, y1, rad1, rad2));
+				line.setAttribute("x1", x1+rad1-svg[0].offsetLeft);
+				line.setAttribute("y1", y1+rad1-svg[0].offsetTop);
+				line.setAttribute("x2", x2+rad2-svg[0].offsetLeft);
+				line.setAttribute("y2", y2+rad2-svg[0].offsetTop);
 				line.setAttribute("style", "stroke: rgb(255,0,0); stroke-width: 3");
-				
-				svg.appendChild(line); 
-				document.body.appendChild(svg);
+				svg[0].appendChild(line);
+
+				var table = document.getElementById("linesTable");
+				var row = table.insertRow(table.rows.length);
+				var cell0 = row.insertCell(0);
+				var cell1 = row.insertCell(1);
+				var cell2 = row.insertCell(2);
+				var cell3 = row.insertCell(3);
+				cell0.innerHTML = x1+rad1;
+				cell1.innerHTML = y1+rad1;
+				cell2.innerHTML = x2+rad2;
+				cell3.innerHTML = y2+rad2;
 			}
 		}
-
 		document.removeEventListener("mouseup", upHandl, true);
 		document.removeEventListener("mousemove", moveHandl, true);
 
